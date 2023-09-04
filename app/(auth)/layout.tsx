d79e7dc6750545396f3aa112712b13213321e35e
Calm Andy <thought7878@qq.com>
2023-09-05 07:49:35 +0800

    init

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+export const metadata = {
+	title: "Social App",
+	description: "A Next.js Social App",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={`${inter.className} bg-dark-1`}>{children}</body>
+			</html>
+		</ClerkProvider>
+	);
+}
