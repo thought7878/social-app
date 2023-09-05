53dfdb405c511238a6d4be4bf66319b42e8deeec
Calm Andy <thought7878@qq.com>
2023-09-05 14:54:37 +0800

    feat(LeftSidebar): create active style

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,18 +1,27 @@
+"use client";
+
 import React from "react";
 import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 const LeftSidebar = () => {
+	const router = useRouter();
+	const pathname = usePathname();
 	return (
 		<section className="custom-scrollbar leftsidebar">
 			<div className="flex w-full flex-1 flex-col gap-6 px-6">
 				{sidebarLinks.map((link) => {
+					const isActive =
+						pathname === link.route ||
+						(pathname.includes(link.route) && link.route.length > 1);
+
 					return (
 						<Link
 							href={link.route}
 							key={link.label}
-							className="leftsidebar_link"
+							className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
 						>
 							<Image
 								src={link.imgURL}
