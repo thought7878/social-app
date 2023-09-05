44c1f6b573c796203db9c79e2b0df683f5986039
Calm Andy <thought7878@qq.com>
2023-09-05 08:44:47 +0800

    init clerk

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,5 @@
+async function Page() {
+	return <main>onboarding</main>;
+}
+
+export default Page;
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import "../globals.css";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			threads
-		</main>
+		<>
+			<h1 className="head-text text-left">Home</h1>
+		</>
 	);
 }
