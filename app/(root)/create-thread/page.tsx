680b5460a756fe4fe6ab4024923dc35694c70ebd
Calm Andy <thought7878@qq.com>
2023-09-07 17:34:35 +0800

    feat(create-thread): init

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,26 @@
+import { currentUser } from "@clerk/nextjs";
+import { fetchUser } from "../../../lib/actions/user";
+import { redirect } from "next/navigation";
+import PostThread from "../../../components/forms/PostThread";
+
+async function Page() {
+	const user = await currentUser();
+	if (!user) {
+		return;
+	}
+
+	const userInfo = await fetchUser(user.id);
+
+	if (!userInfo?.onboarded) {
+		redirect("/onboarding");
+	}
+
+	return (
+		<>
+			<h1 className="head-text">Create Thread</h1>
+			<PostThread userId={userInfo._id} />
+		</>
+	);
+}
+
+export default Page;
---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,5 @@
+function PostThread({ userId }: { userId: string }) {
+	return <section>Post thread form</section>;
+}
+
+export default PostThread;
