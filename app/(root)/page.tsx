4c9be3d224a9cd24d2f5375d32d69cb86603a99d
Calm Andy <thought7878@qq.com>
2023-09-08 15:46:38 +0800

    feat(fetch threads): add backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import { fetchThreads } from "../../lib/actions/thread";
 
-export default function Home() {
+export default async function Home() {
+	const threads = await fetchThreads(1, 10);
+
+	console.log(threads);
+
 	return (
 		<div>
 			{/* <UserButton afterSignOutUrl="/" /> */}
--- lib/actions/thread.ts
+++ lib/actions/thread.ts
@@ -33,4 +33,40 @@ async function createThread({ text, author, communityId, path }: Params) {
 	}
 }
 
-export { createThread };
+async function fetchThreads(pageNumber = 1, pageSize = 20) {
+	try {
+		connectToDB();
+
+		const skipAmount = (pageNumber - 1) * pageSize;
+
+		// find top level threads that have no parents
+		const threadQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+			.sort({
+				createdAt: "desc",
+			})
+			.skip(skipAmount)
+			.limit(pageSize)
+			.populate({ path: "author", model: User })
+			.populate({
+				path: "children",
+				populate: {
+					path: "author",
+					model: User,
+					select: "_id name parentId image",
+				},
+			});
+
+		const totalThreadsCount = await Thread.countDocuments({
+			parentId: { $in: [null, undefined] },
+		});
+
+		const threads = await threadQuery.exec();
+		const hasNext = totalThreadsCount > skipAmount + threads.length;
+
+		return { threads, hasNext };
+	} catch (error: any) {
+		throw new Error(`Failed to fetch threads: ${error.message}`);
+	}
+}
+
+export { createThread, fetchThreads };
