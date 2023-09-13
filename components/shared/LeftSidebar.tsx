1ced06623c83146adf9f1bd60d6dbdcfef56a21c
Calm Andy <thought7878@qq.com>
2023-09-13 15:20:13 +0800

    feat(LeftSidebar): create profile link

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -5,11 +5,13 @@ import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
 const LeftSidebar = () => {
 	const router = useRouter();
 	const pathname = usePathname();
+	const { userId } = useAuth();
+
 	return (
 		<section className="custom-scrollbar leftsidebar">
 			<div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -17,6 +19,10 @@ const LeftSidebar = () => {
 					const isActive =
 						pathname === link.route ||
 						(pathname.includes(link.route) && link.route.length > 1);
+
+					if (link.route === "/profile") {
+						link.route = `${link.route}/${userId}`;
+					}
 
 					return (
 						<Link
